7a734c1860a4556eecb51f541f2b914d8e9d090e
jecsacuan <jec@sacuan.com>
2016-04-11 12:53:43 +0800

    Implement test fragments

--- app/src/main/java/com/tincan/tincan/MainActivity.java
+++ app/src/main/java/com/tincan/tincan/MainActivity.java
@@ -1,14 +1,89 @@
 package com.tincan.tincan;
 
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.transition.ActionBarTransition;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
+    ViewPager mViewPager;
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, switch to the corresponding page in the ViewPager.
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    public static class MainAdapter extends FragmentPagerAdapter{
+        public MainAdapter(FragmentManager fm){
+            super(fm);
+        }
+        @Override
+        public Fragment getItem(int i) {
+            switch (i){
+                case 0:
+                    return new SpeedDialFragment();
+                default:
+                    Fragment fragment = new DummySectionFragment();
+                    Bundle args = new Bundle();
+                    args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, i + 1);
+                    fragment.setArguments(args);
+                    return fragment;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
+
+    public static class SpeedDialFragment extends Fragment{
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+            View rootView = inflater.inflate(R.layout.fragment_speed, container, false);
+            rootView.findViewById(R.id.demo_collection_button)
+                    .setOnClickListener(new View.OnClickListener(){
+                       @Override
+                        public void onClick(View view){
+                           Intent intent = new Intent(getActivity(), SpeedDial.class);
+                           startActivity(intent);
+                       }
+                    });
+            return rootView;
+        }
+    }
+    public static class DummySectionFragment extends Fragment{
+        public static final String ARG_SECTION_NUMBER = "section_number";
+    }
+    /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +100,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+*/
 }
